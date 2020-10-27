ff3804c8ac5bdd54075eaff33536d66d08da0558
Overkane <overkane@mail.ru>
2020-10-27 18:22:04 +0500

    Finished Colors&Type page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './blocks/color-square/color-square.scss';
 import './_includes/ui-kit-header/ui-kit-header.scss';
-import 'normalize.css';
+import './_includes/ui-kit-header/logo.svg';
 import './styles.scss';
