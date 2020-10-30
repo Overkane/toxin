efc9c2c6db0e7ee4688833c4233bf2ea94f3ccad
Overkane <overkane@mail.ru>
2020-10-30 21:29:04 +0500

    Made some components for forms

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import 'Components/color-square/color-square.scss';
 import 'Components/text-field/text-field.scss';
+import 'Components/toggle/toggle.scss';
+import 'Components/info-card/info-card.scss';
+import 'Components/info-card/city.svg';
+import 'Components/info-card/emoticon.svg';
+import 'Components/radio/radio.scss';
+import 'Components/like-button/like-button.scss';
 import 'Components/checkbox/checkbox.scss';
 import 'Components/checkbox/checkbox-flag.svg';
 import 'Components/checkbox-list/checkbox-list.scss';
+import 'Components/bullet-list/bullet-list.scss';
+import 'Components/bullet-list/bullet.svg';
 import 'Includes/ui-kit-header/ui-kit-header.scss';
 import 'Includes/ui-kit-header/logo.svg';
 import 'Pages/ui-kit/form-elements/form-elements.scss';
